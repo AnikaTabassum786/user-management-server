bc853a69e3788b4825fbbca0c05cceaf2c94e20d
AnikaTabassum217 <tabassum.anika579@gmail.com>
2025-05-18 16:10:48 +0600

    Update added

--- index.js
+++ index.js
@@ -47,13 +47,26 @@ async function run() {
             res.send(result)
         })
 
-        app.delete('/users/:id',async(req,res)=>{
+        app.delete('/user/:id',async(req,res)=>{
             const id = req.params.id
-            const query={_id: new ObjectId(id)}
+           
+            const query = { _id: new ObjectId(id) };
             const result= await userCollection.deleteOne(query)
             res.send(result)
         })
 
+        app.put('/users/:id',async(req,res)=>{
+            const id = req.params.id;
+            const filter={_id: new ObjectId(id)}
+            const updatedUser =req.body
+            const updateDoc={
+                $set:updatedUser
+            }
+            const result = await userCollection.updateOne(filter,updateDoc)
+            res.send(result)
+        })
+
+  
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
